b2f4d7e6710f16863daa7a11b25558dff2e9dd19
Ishita <iamod@tekion.com>
2023-07-06 21:18:28 +0530

    first commit

---
+++ src/components/RowSelection.js
@@ -0,0 +1,189 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { useTable, useRowSelect } from "react-table";
+import Select from "react-select";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import "./table.css";
+import Checkbox from "../components/Checkbox/Checkbox";
+import Modal from "../components/Modal/Modal";
+
+const RowSelection = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const [tableData, setTableData] = useState(data);
+  const [onClick, setOnClick] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [filteredRows, setFilteredRows] = useState([]);
+
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => [
+        {
+          id: "selection",
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <Checkbox {...getToggleAllRowsSelectedProps()} />
+          ),
+          Cell: ({ row }) => (
+            <Checkbox {...row.getToggleRowSelectedProps()} />
+          ),
+        },
+        ...columns,
+      ]);
+    }
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    selectedFlatRows,
+    prepareRow,
+  } = tableInstance;
+
+  const firstPageRows = rows.slice(0, 10);
+
+  const companyOptions = [
+    { label: "Zazio", value: "Zazio" },
+    { label: "Gabvine", value: "Gabvine" },
+    { label: "Gabtype", value: "Gabtype" },
+    { label: "Flipbug", value: "Flipbug" },
+    { label: "Mynte", value: "Mynte" },
+  ];
+
+  const options = [
+    { value: "Active", label: "Active" },
+    { value: "Closed", label: "Closed" },
+  ];
+
+  const handleAddMembers = () => {
+    setOnClick(true);
+  };
+
+  const filterRows = (selectedOption, selectId) => {
+    if (selectId === "select1") {
+      const filtered = rows.filter(
+        (row) => row.original.Company === selectedOption.value
+      );
+      setFilteredRows(filtered);
+    }
+
+    if (selectId === "select2") {
+      const filtered = rows.filter(
+        (row) => row.original.Status === selectedOption.value
+      );
+      setFilteredRows(filtered);
+    }
+  };
+
+  const deleteRow = useCallback((row) => {
+    console.log(row);
+    const updatedRows = tableData.filter((dataRow) => dataRow !== row.original);
+    setTableData(updatedRows);
+    setFilteredRows(filteredRows.filter((filteredRow) => filteredRow !== row.original));
+  },[tableData]);
+
+  const handleOnChange = (selectedOption, selectId) => {
+    if (selectId === "select1") {
+      setSelectedOption(selectedOption);
+      filterRows(selectedOption, selectId);
+    }
+
+    if (selectId === "select2") {
+      setSelectedOption(selectedOption);
+      filterRows(selectedOption, selectId);
+    }
+  };
+
+  const CheckboxOption = ({ children, isSelected, innerProps }) => (
+    <div {...innerProps} className="checkbox-filter">
+      <input type="checkbox" checked={isSelected} readOnly />
+      <label>{children}</label>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="heading-container">
+        <div className="heading">Team Members</div>
+        <button className="add-button" onClick={handleAddMembers}>
+          Add Members +
+        </button>
+      </div>
+
+      <div className="filter-dropdown">
+        <Select
+          placeholder="Company"
+          options={companyOptions}
+          value={selectedOption}
+          onChange={(selectedOption) =>
+            handleOnChange(selectedOption, "select1")
+          }
+          closeMenuOnSelect={false}
+          components={{ Option: CheckboxOption }}
+          isMulti
+        />
+
+        <Select
+          options={options}
+          value={selectedOption}
+          onChange={(selectedOption) =>
+            handleOnChange(selectedOption, "select2")
+          }
+          placeholder="Status"
+        />
+      </div>
+
+      <br />
+      {onClick && <Modal onClick={onClick} setOnClick={setOnClick} />}
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {filteredRows.length > 0
+            ? filteredRows.map((row, index) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()} key={index}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    ))}
+                    <td>
+                      <button onClick={() => deleteRow(row)}>delete</button>
+                    </td>
+                  </tr>
+                );
+              })
+            : firstPageRows.map((row, index) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()} key={index}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    ))}
+                    <td>
+                      <button onClick={() => deleteRow(row)}>delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default RowSelection;
---
+++ src/components/columns.js
@@ -0,0 +1,76 @@
+import {format} from 'date-fns';
+
+export const COLUMNS = [
+  {
+    Header: "Name",
+    accessor: "Name",
+    disableFilters : true
+  },
+  {
+    Header: "Company",
+    accessor: "Company",
+ 
+  },
+  {
+    Header: "Status",
+    accessor: "Status",
+  
+  },
+  {
+    Header: "Last Updated",
+    accessor: "Last_Updated",
+    Cell : ({value})=>{return format(new Date(value), 'd/m/yyyy')},
+   
+  },
+  {
+    Header: "Notes",
+    accessor: "Notes",
+   
+  },
+  
+];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "Id",
+    Footer: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        Footer: "Date of Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Country",
+        Footer: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "Phone",
+        Footer: "Phone",
+        accessor: "phone",
+      },
+    ],
+  },
+];
